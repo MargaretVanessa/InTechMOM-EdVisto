98bb98181732c48e2afeecb04aa7d50bf12decb4
Margaret Vanessa Molina Urrutia <mvmolina@prismamp.com>
2023-03-15 18:42:21 -0300

    segundo commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 function loadfileVideo(input_id, video_id) {
-    var fileInput = document.getElementById(input_id);
-    var fileUrl = window.URL.createObjectURL(fileInput.files[0]);
-    document.getElementById(video_id).src = fileUrl;
-  }
+  var fileInput = document.getElementById(input_id);
+  var fileUrl = window.URL.createObjectURL(fileInput.files[0]);
+  document.getElementById(video_id).src = fileUrl;
+}
